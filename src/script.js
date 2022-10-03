b2a79b8b9f54364cc78abe8ecd7879c4a0f419d8
Staffan Ösp <staffan.osp@gmail.com>
2022-10-03 18:51:42 +0200

    Calculate the radius of the menu clip circle dynamically

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 const navToggle = () => {
-  nav = document.getElementById("navigation");
+  const nav = document.getElementById("navigation");
   nav.classList.toggle("navigation--open");
 };
 
@@ -13,3 +13,25 @@ let navMenuToggleItems = document.querySelectorAll(".navigation__menu-toggle");
 for (const navMenuToggleItem of navMenuToggleItems) {
   navMenuToggleItem.addEventListener("click", navToggle);
 }
+
+//Calculate the radius for the hamburger menu clip path
+
+const updateMenuClipCircle = () => {
+  const root = document.querySelector(":root");
+
+  const circleSize = 32; //Todo: read from CSS
+  const circleMargin = 16; //Todo: read from CSS
+
+  const x = window.innerWidth - (circleSize + circleMargin);
+  const y = window.innerHeight - (circleSize + circleMargin);
+
+  const r = Math.sqrt(y ** 2 + x ** 2);
+
+  root.style.setProperty("--navigation__circle-size--open", `${r}px`);
+};
+
+updateMenuClipCircle();
+
+window.addEventListener("resize", () => {
+  updateMenuClipCircle();
+});
